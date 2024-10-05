5a4ab0e2b6e7e8d017e0e4af78f75653d09c25e1
karik-ribasu <mechamocaique@gmail.com>
2024-10-05 03:10:24 -0300

    feat: add contribute function

--- contracts/Capmaign.sol
+++ contracts/Capmaign.sol
@@ -32,4 +32,26 @@ contract Campaign {
         createdAt = block.timestamp;
         isActive = true;
     }
+
+        function contribute(uint256 amount, address token) public payable{
+        require(isActive, "Campaign is not active");
+        require(block.timestamp < deadline, "Campaign has ended");
+        require((amount > 0 && token != address(0) && msg.value == 0) || (msg.value > 0 && token == address(0)), "Is not allowed to send native currency and erc20 tokens at once");
+
+        if (token != address(0) || msg.value == 0) {
+            IERC20 ierc20Token = IERC20(token);
+            require(ierc20Token.allowance(msg.sender, address(this)) >= amount, "Insufficient allowance");
+            ierc20Token.transferFrom(msg.sender, address(this), amount);
+            tokenBalance[token] += amount;
+            allowedApprovers[msg.sender] = true;
+            contributers[msg.sender][token] += amount;
+            emit Contribution(msg.sender, amount, token, block.timestamp);
+        } else {
+            require(msg.value >= minimumContribution, "Contribution must meet the minimum requirement");
+            tokenBalance[address(0)] += msg.value; // increase native token balance
+            allowedApprovers[msg.sender] = true;
+            contributers[msg.sender][address(0)] += msg.value;
+            emit Contribution(msg.sender, msg.value, token, block.timestamp);
+        }
+    }
 }
